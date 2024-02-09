d471b09d6079aec9ae437645c4448800bd3e3765
DGRcodex <d.g.r.developer@gmail.com>
2024-02-09 12:10:31 -0300

    nofooter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Inter } from 'next/font/google';
-import Navbar from './components/Navbar'; // Ajusta esta ruta según sea necesario
-import Footer from './components/Footer'; // Ajusta esta ruta según sea necesario
+
 import './globals.css';
 import Head from 'next/head';
 
@@ -18,9 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       {/* Aquí puedes agregar otros elementos de metadata relevantes */}
     </Head>
 
-      <Navbar />
-      <div className={inter.className}>{children}</div>
-      <Footer />
+    
     </>
   );
 }
