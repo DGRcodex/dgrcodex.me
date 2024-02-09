6976f7b957986507fa7a7b0d63900ffb09511b32
DGRcodex <d.g.r.developer@gmail.com>
2024-02-09 12:29:12 -0300

    el problema estaba en layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Inter } from 'next/font/google';
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-import './globals.css';
-import Head from 'next/head';
+const inter = Inter({ subsets: ['latin'] })
 
-const inter = Inter({ subsets: ['latin'] });
+export const metadata: Metadata = {
+  title: 'Daniel Garcia Rojas Portfolio',
+  description: 'DGRcodex',
+}
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <>      
-    <Head>
-      <title>DGRcodex Portfolio</title>
-      <meta name="description" content="Daniel García Rojas, Desarrollador Fullstack, Fullstack developer, DGRcodex, Portfolio" />
-      <meta name="keywords" content="Fullstack, Desarrollador, Portfolio, Tecnología, Programación, developmeant" />
-      <meta name="author" content="SAMbalab" />
-      {/* Aquí puedes agregar otros elementos de metadata relevantes */}
-    </Head>
-
-    
-    </>
-  );
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
 }
