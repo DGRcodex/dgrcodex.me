322ec0f347d4e8a112ebd8ea0621e7fd0010e221
DGRcodex <d.g.r.developer@gmail.com>
2024-02-09 14:36:20 -0300

    fondo

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-b from-sky-200 to-blue-200 dark:from-sky-900 dark:to-[#0141ff]">
       <Image
-        src="/background-image.jpg"
+        src="/portafolio2.png"
         alt="Background image"
         layout="fill"
         objectFit="cover"
         quality={100}
       />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="text-4xl font-bold text-white dark:text-gray-100">
+        <p className="text-4xl font-bold text-black dark:text-gray-100">
           dgrcodex.me está en construcción por SAMBALAB
         </p>
         <div className="flex place-items-center gap-4 p-4">
