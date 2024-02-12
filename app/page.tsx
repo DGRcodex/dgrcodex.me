08ca7682d2003f5cfcfb1b54bc09d995be98d795
DGRcodex <d.g.r.developer@gmail.com>
2024-02-12 17:05:17 -0300

    aboutme and personal image

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,44 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-      <><Head>
-      <title>Portafolio | Daniel Garcia Rojas</title>
-    </Head><main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-b from-sky-200 to-blue-200 dark:from-sky-900 dark:to-[#0141ff]">
-        <Image
-          src="/portafolio2.png"
-          alt="Background image"
-          layout="fill"
-          objectFit="cover"
-          quality={100} />
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <p className="text-4xl font-bold text-black dark:text-gray-100">
-            dgrcodex.me está en construcción por SAMBALAB
-          </p>
-          <div className="flex place-items-center gap-4 p-4">
+    <>
+      <Head>
+        <title>Portafolio | Daniel Garcia Rojas</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gradient-to-b from-sky-200 to-blue-200 dark:from-sky-900 dark:to-[#0141ff]">
+        <div className="container mx-auto"> {/* Contenedor para centrar los elementos */}
+          <div className="flex flex-col md:flex-row items-center gap-10"> {/* Disposición del contenido */}
+            {/* Columna 1: Imagen personal */}
+            <div className="w-full md:w-1/3">
+              <Image
+                src="/yosinfondo.png" // Reemplaza con la ruta de tu imagen
+                alt="Tu imagen personal"
+                width={300} // Ajusta el ancho según prefieras
+                height={300} // Ajusta la altura según prefieras
+                layout="responsive" // La imagen se adapta al tamaño disponible
+                priority
+              />
+            </div>
+
+            {/* Columna 2: Texto */}
+            <div className="w-full md:w-2/3 text-black dark:text-gray-100">
+              <h1 className="text-4xl font-bold">ABOUT ME</h1>
+              <p className="text-base text-justify">
+                I've had a dynamic career journey spanning almost a decade. Initially immersed in cultural management and production, I made a pivotal decision in 2020 to transition into web development, leveraging my prior experience with WordPress and my innate fascination with technology and product development.
+
+                Since then, I've been deeply involved in various projects, from co-founding Sambalab in 2021 to contributing as a backend developer at Devsafio, where I specialized in data architecture and user model development using technologies like JavaScript, Express, Sequelize, Docker, and MySQL.
+
+                My current role involves leading a talented team in crafting a cutting-edge cultural platform with Next.js, where we prioritize innovation and responsiveness to the evolving needs of our audience and contemporary artists.
+
+                My technical proficiency encompasses a range of programming languages, frameworks, libraries, and tools, including Ruby, TypeScript, React, Node.js, Docker, PostgreSQL, and more. Alongside my professional endeavors, I've pursued education in diverse fields, from digital startups to music composition and legal studies.
+
+                Continuously seeking growth, I'm currently expanding my skill set through courses in data analysis and Python programming. Fluent in Spanish and proficient in English, I thrive on challenges and am passionate about pushing the boundaries of what's possible in the digital realm.
+              </p>
+            </div>
+          </div>
+
+          {/* Logos al final de la página (opcional) */}
+          <div className="flex place-items-center gap-4 p-4 mt-10">
             <Image
               src="/dgrcodex4.png"
               alt="DGR CODEX Logo"
@@ -31,6 +55,7 @@ export default function Home() {
               priority />
           </div>
         </div>
-      </main></>
+      </main>
+    </>
   );
 }
